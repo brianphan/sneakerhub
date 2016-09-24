bb91e285c3c974f23657c39a1c42aae34451adab
Brian Phan <phanbria@phantastik-MBP.local>
2016-09-24 14:21:00 -0400

    implemented sneaker model

--- main_app/models.py
+++ main_app/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Sneaker(models.Model):
+	name = models.CharField(max_length=100)
+	brand = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=100, decimal_places=2)
+	img_url = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.name
